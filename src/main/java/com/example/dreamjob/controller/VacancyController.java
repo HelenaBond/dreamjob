73f4aa22144119e98f30dbc6ebdf2f756f1bf6f9
Elena <bond.elena.15.05@gmail.com>
2025-05-18 21:48:52 +0300

    4. Thymeleaf, Циклы. [#504841]

---
+++ src/main/java/com/example/dreamjob/controller/VacancyController.java
@@ -0,0 +1,21 @@
+package com.example.dreamjob.controller;
+
+import com.example.dreamjob.repository.MemoryVacancyRepository;
+import com.example.dreamjob.repository.VacancyRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vacancies") /* Работать с кандидатами будем по URI /vacancies/** */
+public class VacancyController {
+
+    private final VacancyRepository vacancyRepository = MemoryVacancyRepository.getInstance();
+
+    @GetMapping
+    public String getAll(Model model) {
+        model.addAttribute("vacancies", vacancyRepository.findAll());
+        return "vacancies/list";
+    }
+}
