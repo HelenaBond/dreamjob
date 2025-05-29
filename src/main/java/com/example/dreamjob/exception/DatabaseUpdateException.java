5806fe6bace640dd263a16146dfb55c4ae419fd6
Elena <bond.elena.15.05@gmail.com>
2025-05-29 16:46:14 +0300

    fix rename

--- src/main/java/com/example/dreamjob/exception/DatabaseUpdateException.java
+++
@@ -1,7 +0,0 @@
-package com.example.dreamjob.exception;
-
-public class DatabaseUpdateException extends RuntimeException {
-    public DatabaseUpdateException(String message) {
-        super(message);
-    }
-}
